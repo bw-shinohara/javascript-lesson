0612fa8ce5ba413efde3e588a5ec0c600938d844
Your Name <you@example.com>
2025-05-22 22:01:54 +0900

    Q1andQ2

---
+++ app.js
@@ -0,0 +1,9 @@
+// Q1変数
+let nickname ='ヨースケ';
+let age = 33;
+console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
+
+// Q2配列
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+let templateText = `私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`;
+console.log(templateText);
