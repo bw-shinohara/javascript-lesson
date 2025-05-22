d2fd813d3c16eb3bbe4561686644c87793570735
Your Name <you@example.com>
2025-05-22 23:00:17 +0900

    Q1~Q6

--- app.js
+++ app.js
@@ -7,3 +7,56 @@ console.log('私のニックネームは' + nickname + 'です。年齢は' + ag
 let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
 let templateText = `私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`;
 console.log(templateText);
+
+// Q3オブジェクト
+let user = {
+  name: 'John',
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+};
+console.log(user.age);
+
+
+// Q4配列×オブジェクト
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+console.log(playerList[1].favorites[1]);
+
+
+// Q5四則演算
+let totalAge = 0;
+for (let player of playerList) {
+  totalAge += player.age;
+}
+let averageAge = totalAge / playerList.length;
+console.log(averageAge);
+
+
+// Q6関数
+function sayHello() {
+  console.log('Hello');
+}
+sayHello();
+
+let sayWorld = function() {
+  console.log('World');
+}
+sayWorld();
+
+
